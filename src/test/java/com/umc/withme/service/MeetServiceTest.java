4a660cf2b12e1af53d8f356b2abcb1e700fb4a57
kang hyeon <hyeonkang@inha.edu>
2023-01-17 21:13:00 +0900

    refactor: #17 MeetServiceTest 리팩토링
    함수 내에서 데이터 셋업하는 코드가 중복되어 이 부분을 따로 함수로 빼주었다.

--- src/test/java/com/umc/withme/service/MeetServiceTest.java
+++ src/test/java/com/umc/withme/service/MeetServiceTest.java
@@ -9,6 +9,7 @@ import com.umc.withme.dto.Meet.MeetDto;
 import com.umc.withme.repository.AddressRepository;
 import com.umc.withme.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,11 +40,9 @@ class MeetServiceTest {
     public void 모임_목록_1개_조회() throws Exception{
         //given
         Address address = addressRepository.findBySidoAndSgg("서울특별시", "종로구").get();
-
-        Member member = createMember("1111@naver.com", "010-1111-1111", Gender.FEMALE, address);
+        Member member = createMember("AAAA@naver.com", "010-AAAA-AAAA", Gender.FEMALE, address);
         memberRepository.save(member);
-
-        Meet saveMeet = createMeet(member, "title1", "www.1111.com", "content1");
+        Meet saveMeet = createMeet(member, "titleA", "www.AAAA.com", "contentA");
         meetService.createMeet(saveMeet);
 
         //when
@@ -65,23 +64,6 @@ class MeetServiceTest {
     @Test
     void 모임_전체_조회_기능_테스트() {
         // given
-        Address address1 = addressRepository.findBySidoAndSgg("서울특별시", "종로구").get();
-        Address address2 = addressRepository.findBySidoAndSgg("서울특별시", "강남구").get();
-        Address address3 = addressRepository.findBySidoAndSgg("부산광역시", "해운대구").get();
-
-        Member member1 = createMember("1111@naver.com", "010-1111-1111", Gender.FEMALE, address1);
-        Member member2 = createMember("2222@naver.com", "010-2222-2222", Gender.FEMALE, address2);
-        Member member3 = createMember("3333@naver.com", "010-3333-3333", Gender.MALE, address3);
-        memberRepository.save(member1);
-        memberRepository.save(member2);
-        memberRepository.save(member3);
-
-        Meet meet1 = createMeet(member1, "title1", "www.1111.com", "content1");
-        Meet meet2 = createMeet(member2, "title2", "www.2222.com", "content2");
-        Meet meet3 = createMeet(member3, "title3", "www.3333.com", "content3");
-        meetService.createMeet(meet1);
-        meetService.createMeet(meet2);
-        meetService.createMeet(meet3);
 
         // when
         List<MeetDto> meetDtos = meetService.findMeets();
@@ -96,6 +78,18 @@ class MeetServiceTest {
     @Test
     public void 제목_으로_모임_리스트_조회() throws Exception{
         //given
+        
+        //when
+        List<MeetDto> findMeetDtos = meetService.findMeetsByTitle("titleA");
+
+        //then
+        assertThat(findMeetDtos.size()).isEqualTo(2);
+        assertThat(findMeetDtos.get(0).getContent()).isEqualTo("content1");
+        assertThat(findMeetDtos.get(1).getContent()).isEqualTo("content3");
+    }
+
+    @BeforeEach
+    public void setUp(){
         Address address1 = addressRepository.findBySidoAndSgg("서울특별시", "종로구").get();
         Address address2 = addressRepository.findBySidoAndSgg("서울특별시", "강남구").get();
         Address address3 = addressRepository.findBySidoAndSgg("부산광역시", "해운대구").get();
@@ -113,14 +107,6 @@ class MeetServiceTest {
         meetService.createMeet(meet1);
         meetService.createMeet(meet2);
         meetService.createMeet(meet3);
-
-        //when
-        List<MeetDto> findMeetDtos = meetService.findMeetsByTitle("titleA");
-
-        //then
-        assertThat(findMeetDtos.size()).isEqualTo(2);
-        assertThat(findMeetDtos.get(0).getContent()).isEqualTo(meet1.getContent());
-        assertThat(findMeetDtos.get(1).getContent()).isEqualTo(meet3.getContent());
     }
 
     private static Meet createMeet(Member member1, String title1, String link, String content1) {
