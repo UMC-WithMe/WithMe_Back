88847209fb7366abeffd93a3f9a19329b41ad1eb
kang hyeon <hyeonkang@inha.edu>
2023-01-17 20:17:07 +0900

    feat: #17 'Meet' Id로 1개 조회 Repository Test
    Repository Layer에서 Meet Id가 주어졌을 때 조회를 잘 하는지 테스트했다.

--- src/test/java/com/umc/withme/repository/MeetRepositoryTest.java
+++ src/test/java/com/umc/withme/repository/MeetRepositoryTest.java
@@ -5,6 +5,7 @@ import com.umc.withme.domain.Meet;
 import com.umc.withme.domain.Member;
 import com.umc.withme.domain.constant.Gender;
 import com.umc.withme.domain.constant.MeetCategory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Meet - Repository Layer Test")
@@ -23,7 +26,33 @@ public class MeetRepositoryTest {
     @Autowired private MeetRepository meetRepository;
     @Autowired private MemberRepository memberRepository;
     @Autowired private AddressRepository addressRepository;
+    
+    @Test
+    public void 모임_목록_1개_조회() throws Exception{
+        //given
+        Address address = addressRepository.findBySidoAndSgg("서울특별시", "종로구").get();
 
+        Member member = createMember("1111@naver.com", "010-1111-1111", Gender.FEMALE, address);
+        memberRepository.save(member);
+
+        Meet saveMeet = createMeet(member, "title1", "www.1111.com", "content1");
+        meetRepository.save(saveMeet);
+        
+        //when
+        Optional<Meet> findMeet = meetRepository.findById(saveMeet.getId());
+
+        //then
+        if(findMeet.isPresent()){
+            Meet meet = findMeet.get();
+            assertThat(meet).isEqualTo(saveMeet);
+            assertThat(meet.getLeader()).isEqualTo(saveMeet.getLeader());
+            assertThat(meet.getTitle()).isEqualTo(saveMeet.getTitle());
+            assertThat(meet.getLink()).isEqualTo(saveMeet.getLink());
+            assertThat(meet.getContent()).isEqualTo(saveMeet.getContent());
+        }else
+            fail("모임이 조회되어야 합니다.");
+    }
+    
     @Test
     void 전체_모임_목록을_조회한다(){
 
