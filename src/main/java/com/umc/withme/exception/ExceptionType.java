ddda709392e255688a4b898581c5b695040f2062
Wo-ogie <siwall0105@gmail.com>
2023-01-18 15:24:48 +0900

    fix: #31 GlobalExceptionHandler에서 ConstraintViolationException을 처리하도록 수정
    기존에 `ConstraintViolationException` 처리에 대한 로직이 부실해서 exception handling이 되지 않고 있었다.
    그 때문에 이를 수정하였음.

--- src/main/java/com/umc/withme/exception/ExceptionType.java
+++ src/main/java/com/umc/withme/exception/ExceptionType.java
@@ -1,5 +1,6 @@
 package com.umc.withme.exception;
 
+import com.umc.withme.exception.address.AddressNotFoundException;
 import com.umc.withme.exception.common.CustomException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,21 +13,16 @@ import java.util.Objects;
 public enum ExceptionType {
 
     // Common
-    UNHANDLED_EXCEPTION(0, "알 수 없는 서버 에러가 발생했습니다."),
-    METHOD_ARGUMENT_NOT_VALID_EXCEPTION(1, "요청 데이터가 잘못되었습니다."),
+    UNHANDLED_EXCEPTION(0, "알 수 없는 서버 에러가 발생했습니다.", null),
+    BAD_REQUEST_EXCEPTION(1, "요청 데이터가 잘못되었습니다.", null),
 
     // Address
-    ADDRESS_NOT_FOUND_EXCEPTION(3400, "일치하는 주소를 찾을 수 없습니다.")
+    ADDRESS_NOT_FOUND_EXCEPTION(3400, "일치하는 주소를 찾을 수 없습니다.", AddressNotFoundException.class),
     ;
 
     private final int errorCode;
     private final String errorMessage;
     private Class<? extends CustomException> type;
-
-    ExceptionType(int errorCode, String errorMessage) {
-        this.errorCode = errorCode;
-        this.errorMessage = errorMessage;
-    }
 
     public static ExceptionType from(Class<?> classType) {
         return Arrays.stream(values())
