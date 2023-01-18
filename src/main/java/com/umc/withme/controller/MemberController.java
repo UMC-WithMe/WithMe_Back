2d57cb320c2262648762002637462a101d5c6d3d
sujeong11 <qalzm7351@gmail.com>
2023-01-18 15:13:20 +0900

    fix: #21 코드리뷰 기반으로 코드 수정

--- src/main/java/com/umc/withme/controller/MemberController.java
+++ src/main/java/com/umc/withme/controller/MemberController.java
@@ -1,19 +1,19 @@
 package com.umc.withme.controller;
 
-import com.umc.withme.dto.common.BaseResponse;
 import com.umc.withme.dto.common.DataResponse;
-import com.umc.withme.dto.common.ErrorResponse;
-import com.umc.withme.exception.ExceptionType;
 import com.umc.withme.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class MemberController {
 
     private final MemberService memberService;
@@ -22,17 +22,16 @@ public class MemberController {
      * 닉네임 중복 조회를 하는 API
      *
      * @param nickname
-     * @return 중복 여부를 포함한 DataResponse
-     * @throws 닉네임 값이 공백일 때 ErrorResponse
+     * @return 중복 여부를 DataResponse에 담아 반환
+     * @throws ConstraintViolationException 닉네임이 공백인 경우
      */
     // TODO: Swagger 문서에 적용
-    @GetMapping("/check/{nickname}/exists")
-    public BaseResponse checkNicknameDuplicate(@PathVariable String nickname) {
-        if (nickname.isBlank()) {
-            ExceptionType error = ExceptionType.MEMBER_NICKNAME_IS_NULL_EXCEPTION;
-            return new ErrorResponse(error.getErrorCode(), error.getErrorMessage());
-        }
-
-        return new DataResponse(memberService.checkNicknameDuplication(nickname));
+    @GetMapping("/check/duplicate")
+    public ResponseEntity<DataResponse> checkNicknameDuplicate(
+            @RequestParam @NotBlank String nickname
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new DataResponse(memberService.checkNicknameDuplication(nickname)));
     }
 }
