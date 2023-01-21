e66a03b7190fc9619e44af24b8ba4e37afd07100
Wo-ogie <siwall0105@gmail.com>
2023-01-22 02:24:27 +0900

    docs: MemberService method 설명 수정 및 link 추가
    Service layer의 method는 controller layer 이외에도 다른 곳에서 사용할 가능성이 있다. 이를 고려하여 설명을 자연스럽게 수정하였음.
    또한 `MemberDto`에 대한 link를 추가하여 method 명세에서 해당 class를 바로 확인할 수 있도록 하였음.

--- src/main/java/com/umc/withme/service/MemberService.java
+++ src/main/java/com/umc/withme/service/MemberService.java
@@ -1,6 +1,5 @@
 package com.umc.withme.service;
 
-import com.umc.withme.domain.Member;
 import com.umc.withme.dto.member.MemberDto;
 import com.umc.withme.exception.member.NicknameNotFoundException;
 import com.umc.withme.repository.MemberRepository;
@@ -16,7 +15,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     /**
-     * DB에서 닉네임 중복여부 확인 후 결과를 컨트롤러에게 반환한다.
+     * DB에서 닉네임 중복 여부 확인 후 결과를 반환한다.
      *
      * @param nickname
      * @return 중복인 경우 - true, 중복이 아닌 경우 - false
@@ -29,7 +28,7 @@ public class MemberService {
      * 특정 닉네임을 가진 회원 정보를 컨트롤러에게 반환한다.
      *
      * @param nickname
-     * @return MemberDto에 회원 정보를 담아 반환
+     * @return {@link MemberDto}에 회원 정보를 담아 반환
      * @throws NicknameNotFoundException 닉네임이 존재하지 않을 경우
      */
     public MemberDto getMemberInfo(String nickname) {
