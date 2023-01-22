e2586320808a300cbbc8148268f954cc581f30f7
Wo-ogie <siwall0105@gmail.com>
2023-01-22 22:21:53 +0900

    feat: #47 Spring AOP를 활용한 logging 환경 구축
    Controller, Service, Repository의 모든 public method에 대해 logging을 적용한다.

---
+++ src/main/java/com/umc/withme/config/log/LogTraceAspect.java
@@ -0,0 +1,37 @@
+package com.umc.withme.config.log;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    @Around(
+            "execution(* com.umc.withme.controller..*(..)) ||" +
+            "execution(* com.umc.withme.service..*(..)) || " +
+            "execution(* com.umc.withme.repository..*(..))"
+    )
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            // Logic call
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
