ba0725f27589b125328f69710713df789e6282d0
Wo-ogie <siwall0105@gmail.com>
2023-01-22 20:47:10 +0900

    feat: #29 인증 기능 구현에 따라 jpa auditing 적용이 가능해졌으므로, 해당 기능 구현
    생성자, 수정자는 로그인을 한, 요청을 보낸 회원의 PK(id)를 기입하여 등록되도록 한다.

---
+++ src/main/java/com/umc/withme/config/JpaConfig.java
@@ -0,0 +1,27 @@
+package com.umc.withme.config;
+
+import com.umc.withme.security.WithMeAppPrinciple;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(WithMeAppPrinciple.class::cast)
+                .map(WithMeAppPrinciple::getMemberId);
+    }
+}
