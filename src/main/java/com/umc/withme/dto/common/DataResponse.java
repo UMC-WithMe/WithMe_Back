b53442d95b4559db7a02c6a7f69ebc793d24916a
Wo-ogie <siwall0105@gmail.com>
2023-01-25 00:12:57 +0900

    docs: base response에 Swagger 명세 작성
    #27 에서 추가했던 base response에 간단하게 Swagger 명세를 작성하였다.

--- src/main/java/com/umc/withme/dto/common/DataResponse.java
+++ src/main/java/com/umc/withme/dto/common/DataResponse.java
@@ -1,10 +1,12 @@
 package com.umc.withme.dto.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class DataResponse<T> extends BaseResponse{
 
+    @Schema(description = "응답 데이터")
     private T data;
 
     public DataResponse(T data) {
