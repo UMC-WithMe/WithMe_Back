4b73af4e80caa80487196011b3e7d8add5929681
Wo-ogie <siwall0105@gmail.com>
2023-01-25 00:16:26 +0900

    feat: #54 회원 폰 번호, 주소 수정 API 구현

--- src/main/java/com/umc/withme/controller/MemberController.java
+++ src/main/java/com/umc/withme/controller/MemberController.java
@@ -1,20 +1,25 @@
 package com.umc.withme.controller;
 
+import com.umc.withme.dto.common.BaseResponse;
 import com.umc.withme.dto.common.DataResponse;
-import com.umc.withme.dto.member.MemberDto;
-import com.umc.withme.dto.member.MemberInfoGetResponse;
-import com.umc.withme.dto.member.NicknameDuplicationCheckResponse;
+import com.umc.withme.dto.member.*;
+import com.umc.withme.security.WithMeAppPrinciple;
 import com.umc.withme.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @RestController
@@ -56,5 +61,47 @@ public class MemberController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new DataResponse<>(response));
+    }
+
+    @Operation(
+            summary = "회원 폰 번호 설정/재설정",
+            description = "<p><code>memberId</code>에 해당하는 회원의 폰 번호를 request body의 <code>phoneNumber</code>로 설정합니다.</p>",
+            security = @SecurityRequirement(name = "access-token")
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "1401: <code>email</code>에 해당하는 회원이 없는 경우", content = @Content)
+    })
+    @PatchMapping("/user/phone-number")
+    public ResponseEntity<BaseResponse> updateMemberPhoneNumber(
+            @Parameter(hidden = true) @AuthenticationPrincipal WithMeAppPrinciple principle,
+            @Valid @RequestBody MemberPhoneNumberUpdateRequest request
+    ) {
+        memberService.updateMemberPhoneNumber(principle.getUsername(), request.getPhoneNumber());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new BaseResponse(true));
+    }
+
+    @Operation(
+            summary = "회원 주소 설정/재설정",
+            description = "<p><code>memberId</code>에 해당하는 회원의 주소 정보를 request body의 <code>sido</code>, <code>sgg</code>로 설정합니다.</p>",
+            security = @SecurityRequirement(name = "access-token")
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "1401: <code>email</code>에 해당하는 회원이 없는 경우", content = @Content)
+    })
+    @PatchMapping("/user/address")
+    public ResponseEntity<BaseResponse> updateMemberAddress(
+            @Parameter(hidden = true) @AuthenticationPrincipal WithMeAppPrinciple principle,
+            @Valid @RequestBody MemberAddressUpdateRequest request
+    ) {
+        memberService.updateMemberAddress(principle.getUsername(), request.getSido(), request.getSgg());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new BaseResponse(true));
     }
 }
