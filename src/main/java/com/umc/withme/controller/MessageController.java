a284d41fad178ce22f156466829ba1068520c06e
dldmldlsy <dldmldlsy@naver.com>
2023-02-01 03:33:22 +0900

    feat: #59 쪽지함 조회 api controller 구현

--- src/main/java/com/umc/withme/controller/MessageController.java
+++ src/main/java/com/umc/withme/controller/MessageController.java
@@ -3,10 +3,13 @@ package com.umc.withme.controller;
 import com.umc.withme.dto.common.DataResponse;
 import com.umc.withme.dto.message.MessageCreateRequest;
 import com.umc.withme.dto.message.MessageCreateResponse;
+import com.umc.withme.dto.message.MessageInfoResponse;
 import com.umc.withme.security.WithMeAppPrinciple;
 import com.umc.withme.service.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,5 +50,20 @@ public class MessageController {
         MessageCreateResponse messageCreateResponse = MessageCreateResponse.of(messageId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(new DataResponse<>(messageCreateResponse));
+    }
+
+    /**
+     *
+     * @param principle
+     * @return 쪽지함 조회 응답 데이터 (회원 프로필 정보, 쪽지 내용, 작성시간)
+     */
+    @Operation(summary = "쪽지함 조회", description = "<p> 회원이 자신의 쪽지함을 조회합니다</p>")
+    @GetMapping("/messages")
+    public ResponseEntity<DataResponse<List<MessageInfoResponse>>> getMessageList(
+            @Parameter(hidden = true) @AuthenticationPrincipal WithMeAppPrinciple principle){
+
+        List<MessageInfoResponse> messageInfoResponseList = messageService.getMessageList(principle.getMemberId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(new DataResponse<>(messageInfoResponseList));
     }
 }
